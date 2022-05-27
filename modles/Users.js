5f008801625eaa4fb404d60a4f109af4f57889b0
Spencer <dresmanns@gmail.com>
2022-05-26 21:50:17 -0400

    update

--- modles/Users.js
+++ modles/Users.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
    email: {
        type: String,
-       required: true,
+       required:[ true, 'please enter your email'],
        unique: true,
-       lowercase: true
+       lowercase: true,
+       validate: [isEmail, 'please enter a valid email']
    },
    password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, 'please enter your pass word'],
+    minLength: [6, 'minimum password length is 6 characters']
    }
+})
+
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 const User = mongoose.model('user', userSchema)
