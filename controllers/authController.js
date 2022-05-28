af0782aab554951e9795a07e1d659bce766e18ad
Spencer <dresmanns@gmail.com>
2022-05-28 15:46:09 -0400

    trying my best

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../modles/Users')
+const jwt = require('jsonwebtoken')
 
 const handleError= (err) =>{
     console.log(err.message, err.code);
@@ -17,6 +18,13 @@ const handleError= (err) =>{
     return errors
 }
 
+const maxAge = 3 * 24 * 60 * 60
+const createToken = (id) => {
+    return jwt.sign({ id }, "it's a secret to everyone", {
+        expiresIn: maxAge
+    })
+}
+
 module.exports.signup_get = (req,res) => {
     res.render('signup');
 }
@@ -27,6 +35,8 @@ module.exports.signup_post = async(req,res) => {
     const {email, password} = req.body;
     try{
        const user = await User.create({email, password})
+       const token = createToken(user_id)
+       res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
        res.status(201).json(user)
     }
      catch(err){
